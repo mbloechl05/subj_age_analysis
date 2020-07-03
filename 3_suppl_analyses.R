3f8d46db2146ce680a45cda9f70e9ed292c9c008
Maria Blöchl <bloechl@cbs.mpg.de>
2020-07-03 17:19:13 +0200

    revise exploratory analyses

--- 3_suppl_analyses.R
+++ 3_suppl_analyses.R
@@ -357,258 +357,13 @@ fit.c <- sem(b3.model.c, data = df, group = "sex", se = "robust", estimator = "M
 summary(fit.c)
 
 
-# Model comparison#
+# Model comparison
 
 anova(fit.uc, fit.c)
 
 
-# 4.2.) Model for males 
-# -----------------------
-
-# 4.2.1) Prepare data
-
-# Create male data set
-df.male <- subset(df.eligible, DhSex == 1)
-
-# Trim variable chronological age
-out <- 3*sd(df.male$ca, na.rm = T) # Calculate +/- 3 SD
-
-sum(df.male$ca < mean(df.male$ca, na.rm = T) - out, na.rm = T) # -3 SD: number of excluded people
-sum(df.male$ca > mean(df.male$ca, na.rm = T) + out, na.rm = T) # +3 SD: number of excluded people
-
-df.male$ca[df.male$ca > mean(df.male$ca, na.rm = T) + out | 
-             df.male$ca < mean(df.male$ca, na.rm = T) - out] <- NA # Mark people as NA
-
-# Trim variable subjective age
-out <- 3*sd(df.male$sa, na.rm = T) # Calculate +/- 3 SD
-
-sum(df.male$sa < mean(df.male$sa, na.rm = T) - out, na.rm = T) # - 3 SD: number of excluded people
-sum(df.male$sa > mean(df.male$sa, na.rm = T) + out, na.rm = T) # + 3 SD: number of excluded people
-
-df.male$sa[df.male$sa > mean(df.male$sa, na.rm = T) + out | 
-        df.male$sa < mean(df.male$sa, na.rm = T) - out] <- NA # Mark people as NA
-
-# Exclude cases from trimmed variables
-df.male <- na.omit(df.male)
-
-# Standardise predictors
-grandmean <- mean(c(df.male$sa, df.male$ca), na.rm = T)
-pooledsd  <- sqrt(((sd(df.male$sa)^2)+(sd(df.male$ca)^2))/2)
-
-df.male$sa.s  <- (df.male$sa-grandmean)/pooledsd
-df.male$ca.s  <- (df.male$ca-grandmean)/pooledsd
-
-# Add squared and interaction terms of the predictors
-df.male$sa.s2 <- df.male$sa.s^2
-df.male$ca.s2 <- df.male$ca.s^2
-df.male$sa.ca <- df.male$sa.s*df.male$ca.s
-
-# Descriptive statistics 
-df.male$sa.bias <- df.male$sa - df.male$ca # substract chron. from subj. age
-describe(df.male[,c("ca", "sa", "ls", "sa.bias")])
-hist(df.male$ca, xlim = c(30, 90),  main = "Men: chron. age")
-hist(df.male$sa, xlim = c(15, 100), main = "Men: subj. age")
-
-
-# 4.2.2) Quantify optimal margin
-
-# Fit model
-b3.fit.male <- sem(b3.model, data = df.male, se = "robust", estimator = "MLR")
-summary(b3.fit.male, ci = T, fit.measures = T)
-
-# Prepare data
-ca.male <- c(40,50,60,70,80,90) # Select chronological ages and put them in a data frame
-ca.male <- data.frame(ca.male)
-
-ca.male$ca.sd <- (ca.male$ca - grandmean)/pooledsd # Standardised chron. age
-
-# Extract PA1 parameters from model fit
-p10 <- b3.fit.male@ParTable$est[38] # p10 --> 38
-p11 <- b3.fit.male@ParTable$est[37] # p11 --> 37
-
-# Run calculations 
-ca.male$sa.sd <- ((ca.male$ca.sd - p10)/p11) # Using PA1, calculate corresponding standardised subj. ages 
-ca.male$sa    <- grandmean + (ca.male$sa.sd*pooledsd) # Unstandard. subjective ages
-ca.male$bi    <- ca.male$ca-ca.male$sa # Calculate difference (i.e. optimal subj. age bias)
-ca.male$bi.2  <- ca.male$sa-ca.male$ca # negative values indicate feeling younger
-ca.male$bi.p  <- (ca.male$sa-ca.male$ca)/ca.male$ca # proportinal values (divided by age)
-
-# Show results
-ca.male
-
-
-# 4.2.3) Inspect parameters and plot model
-
-### 3D RSA plot
-r.fu.male  <- RSA(ls  ~ sa.s*ca.s, df.male)
-
-plot(r.fu.male, model = "full",
-     axes = c("LOC", "LOIC", "PA1"),
-     axesStyles = list(LOC  = list(lty = "solid", lwd = 2, col = "black"),
-                       LOIC = list(lty = "solid", lwd = 2, col = "black"),
-                       PA1  = list(lty = "dotted", lwd = 2, col = "grey40")),
-     xlab = "Subjective age", 
-     ylab = "Chronlogical age", 
-     zlab ="Life satisfaction",
-     cex.tickLabel = 2, cex.axesLabel = 2,
-     rotation = list(x = -48, y = 26, z = 20),
-     label.rotation = list(x = 22, y = -51, z = 92),
-     points = list(show = F), hull = T, legend = T,
-     project = c("LOC", "LOIC", "PA1", "points"), param = F,
-     pal = colorRampPalette(c("#24353f", "#385061", "#50748D", "#6b8a9f", 
-                              "#7d98ab", "#9bafbe", "#bbc8d3", "#cad4dc", 
-                              "#e8ecef", "#f0f2f4", "#ffffff"))(14))
-
-# Examine how many points lay below and above first principal axis
-
-# First plot PA1, LOC and dots with quadrants
-plot(df.male$ca.s ~ jitter(df.male$sa.s, 3), pch = 16, xlab = "Subjective age",
-     ylab = "Chronological age", col = rgb(0,0,0, alpha = 0.3))
-abline(a = 3.276, b = 1.390, col = "mediumvioletred", lwd = 3)
-abline(a = 0, b = 1, col = "green4", lwd = 3)
-abline(v = 0, lty = "dotted", lwd = 3)
-abline(h = 0, lty = "dotted", lwd = 3)
-
-# Now calculate how many values are below and above the PA1
-fpa.ca.fit <- 3.276 + 1.390 * df.male$sa.s
-resi <- df.male$ca.s - fpa.ca.fit
-
-sum(resi < 0) # below
-sum(resi > 0) # above
-
-## Check also how many values are below and above the LOC
-loc.ca.fit <- 0 + 1 * df.male$sa.s
-resi <- df.male$ca.s - loc.ca.fit
-
-sum(resi < 0) # below
-sum(resi > 0) # above
-
-
-# 4.3.) Model for females
-# -------------------------
-
-# 4.3.1) Prepare data
-
-# Create male data set
-df.female <- subset(df.eligible, DhSex == 2)
-
-# Trim variable chronological age
-out <- 3*sd(df.female$ca, na.rm = T) # Calculate +/- 3 SD
-
-sum(df.female$ca < mean(df.female$ca, na.rm = T) - out, na.rm = T) # -3 SD: number of excluded people
-sum(df.female$ca > mean(df.female$ca, na.rm = T) + out, na.rm = T) # +3 SD: number of excluded people
-
-df.female$ca[df.female$ca > mean(df.female$ca, na.rm = T) + out | 
-             df.female$ca < mean(df.female$ca, na.rm = T) - out] <- NA # Mark people as NA
-
-# Trim variable subjective age
-out <- 3*sd(df.female$sa, na.rm = T) # Calculate +/- 3 SD
-
-sum(df.female$sa < mean(df.female$sa, na.rm = T) - out, na.rm = T) # - 3 SD: number of excluded people
-sum(df.female$sa > mean(df.female$sa, na.rm = T) + out, na.rm = T) # + 3 SD: number of excluded people
-
-df.female$sa[df.female$sa > mean(df.female$sa, na.rm = T) + out | 
-             df.female$sa < mean(df.female$sa, na.rm = T) - out] <- NA # Mark people as NA
-
-# Exclude cases from trimmed variables
-df.female <- na.omit(df.female)
-
-# Standardise predictors
-grandmean <- mean(c(df.female$sa, df.female$ca), na.rm = T)
-pooledsd  <- sqrt(((sd(df.female$sa)^2)+(sd(df.female$ca)^2))/2)
-
-df.female$sa.s  <- (df.female$sa-grandmean)/pooledsd
-df.female$ca.s  <- (df.female$ca-grandmean)/pooledsd
-
-# Add squared and interaction terms of the predictors
-df.female$sa.s2 <- df.female$sa.s^2
-df.female$ca.s2 <- df.female$ca.s^2
-df.female$sa.ca <- df.female$sa.s*df.female$ca.s
-
-# Descriptive statistics 
-df.female$sa.bias <- df.female$sa - df.female$ca # substract chron. from subj. age
-describe(df.female[,c("ca", "sa", "ls", "sa.bias")])
-hist(df.female$ca, xlim = c(30, 90), main = "Women: chron. age")
-hist(df.female$sa, xlim = c(15, 100), main = "Women: subj. age")
-
-
-# 4.3.2) Quantify optimal margin
-
-# Fit model
-b3.fit.female <- sem(b3.model, data = df.female, se = "robust", estimator = "MLR") 
-summary(b3.fit.female, ci = T, fit.measures = T) 
-
-# Prepare data
-ca <- c(40,50,60,70,80,90) # Select chronological ages and put them in a data frame
-ca.female <- data.frame(ca)
-
-ca.female$ca.sd <- (ca.female$ca - grandmean)/pooledsd # Standardised chron. age
-
-# Extract PA1 parameters from model fit
-p10 <- b3.fit.female@ParTable$est[38] # p10
-p11 <- b3.fit.female@ParTable$est[37] # p11
-
-# Run calculations 
-ca.female$sa.sd <- ((ca.female$ca.sd - p10)/p11) # Using PA1, calculate corresponding standardised subj. ages 
-ca.female$sa    <- grandmean + (ca.female$sa.sd*pooledsd) # Unstandard. subjective ages
-ca.female$bi    <- ca.female$ca-ca.female$sa # Calculate difference (i.e. optimal subj. age bias)
-ca.female$bi.2  <- ca.female$sa-ca.female$ca # negative values indicate feeling younger
-ca.female$bi.p  <- (ca.female$sa-ca.female$ca)/ca.female$ca # proportinal values (divided by age)
-
-# Show results
-ca.female
-
-
-# 4.3.3) Inspect parameters and plot model
-
-### 3D RSA plot
-r.fu.female  <- RSA(ls  ~ sa.s*ca.s, df.female)
-
-plot(r.fu.female, model = "full",
-     axes = c("LOC", "LOIC", "PA1"),
-     axesStyles = list(LOC  = list(lty = "solid", lwd = 2, col = "black"),
-                       LOIC = list(lty = "solid", lwd = 2, col = "black"),
-                       PA1  = list(lty = "dotted", lwd = 2, col = "grey40")),
-     xlab = "Subjective age", 
-     ylab = "Chronlogical age", 
-     zlab ="Life satisfaction",
-     cex.tickLabel = 2, cex.axesLabel = 2,
-     rotation = list(x = -48, y = 26, z = 20),
-     label.rotation=list(x = 22, y = -51, z = 92),
-     points = list(show = F), hull = T, legend = T,
-     project = c("LOC", "LOIC", "PA1", "points"), param = F,
-     pal = colorRampPalette(c("#24353f", "#385061", "#50748D", "#6b8a9f", 
-                              "#7d98ab", "#9bafbe", "#bbc8d3", "#cad4dc", 
-                              "#e8ecef", "#f0f2f4", "#ffffff"))(14))
-
-# First plot PA1, LOC and dots with quadrants
-plot(df.female$ca.s ~ jitter(df.female$sa.s, 3), pch = 16, xlab = "Subjective age",
-     ylab = "Chronological age", col = rgb(0,0,0, alpha = 0.3))
-abline(a = 2.344, b = 1.144, col = "mediumvioletred", lwd = 3)
-abline(a = 0, b = 1, col = "green4", lwd = 3)
-abline(v = 0, lty = "dotted", lwd = 3)
-abline(h = 0, lty = "dotted", lwd = 3)
-
-# Now calculate how many values are below and above the PA1
-fpa.ca.fit <- 2.344 + 1.144 * df.female$sa.s
-resi <- df.female$ca.s - fpa.ca.fit
-
-sum(resi < 0) # below
-sum(resi > 0) # above
-
-## Check also how many values are below and above the LOC
-loc.ca.fit <- 0 + 1 * df.female$sa.s
-resi <- df.female$ca.s - loc.ca.fit
-
-sum(resi < 0) # below
-sum(resi > 0) # above
-
-
-
-
-
-
-
+# 4.2.) Optimal margins for men and women
+# -----------------------------------------
 
 # Prepare data
 ca <- c(40,50,60,70,80,90) # Select chronological ages and put them in a data frame
@@ -650,3 +405,26 @@ ca.female$bi.p  <- (ca.female$sa-ca.female$ca)/ca.female$ca # proportinal values
 
 # Show results
 ca.female
+
+
+# ------------------------------------------------------------
+# 5) "Classical" difference score model and model comparison
+# ------------------------------------------------------------
+
+# Model S1B: Low subj. age and high chronological age model
+
+trad.diff.model <- "
+ls ~ 1 + b1*sa.s + b2*ca.s + + b3*sa.s2 + b4*sa.ca + b5*ca.s2 
+
+# parameter constraints
+b1 == -b2
+b3 == 0
+b4 == 0
+b5 == 0"
+
+fit.trad.diff <- sem(trad.diff.model, data = df, se = "robust", estimator = "MLR")
+summary(fit.trad.diff)
+
+fitmeasures(fit.trad.diff, fit.measures = c("AIC", "CFI", "SRMR", "RMSEA"))
+inspect(fit.trad.diff, 'r2')
+
